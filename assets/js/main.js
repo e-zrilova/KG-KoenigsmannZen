08a42067283559111f8b7aefc7dde39d013a4efc
Elizaveta <karnizmrkiz@mail.ru>
2022-05-21 14:08:27 +0300

    create: section gallery-zen-slider, photos-of-buyers

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,31 @@ const videoMaterialslSlider = new Swiper('.js-video-materials-slider', {
   spaceBetween: 30,
   slidesPerView: 3,
 });
+const galleryZenSlider = new Swiper('.js-gallery-zen-slider', {
+  // Optional parameters
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.js-gallery-zen-slider---next',
+    prevEl: '.js-gallery-zen-slider---prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  slidesPerView: 1,
+});
+
+const photosOfBuyers = new Swiper(".js-photos-of-buyers-slider", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".js-photos-of-buyers-slider--next",
+    prevEl: ".js-photos-of-buyers-slider--prev",
+  },
+  mousewheel: true,
+  keyboard: true,
+  spaceBetween: 64,
+  slidesPerView: 6,
+});
